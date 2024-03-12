e24e55da3ff959ab118db3ee074b80ee35d0c9a1
Chris Macklin <chris.macklin@10xgenomics.com>
2024-03-12 14:28:51 -0700

    Delete the toy_com CLI option. (#392)

--- enclone_args/src/proc_args.rs
+++ enclone_args/src/proc_args.rs
@@ -495,7 +495,6 @@ pub fn proc_args(ctl: &mut EncloneControl, args: &[String]) -> Result<(), String
             &mut ctl.plot_opt.plot_by_isotype_nolegend,
         ),
         ("TOY", &mut ctl.gen_opt.toy),
-        ("TOY_COM", &mut ctl.gen_opt.toy_com),
         ("UMI_FILT_MARK", &mut ctl.clono_filt_opt_def.umi_filt_mark),
         (
             "UMI_RATIO_FILT_MARK",
