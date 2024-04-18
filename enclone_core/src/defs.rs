653db37e4056f28efee0e5bc78f16f529543bd7e
Chris Macklin <chris.macklin@10xgenomics.com>
2024-04-18 13:16:20 -0700

    Delete unused JoinAlgOpt field.

--- enclone_core/src/defs.rs
+++ enclone_core/src/defs.rs
@@ -494,7 +494,6 @@ pub struct JoinAlgOpt {
     pub basicx: bool,
     pub join_full_diff: bool,
     pub join_cdr3_ident: f64,
-    pub join_cdr12h_ident: f64,
     pub fwr1_cdr12_delta: f64,
     pub cdr3_normal_len: usize,
     pub auto_share: usize,
